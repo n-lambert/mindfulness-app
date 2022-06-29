7d8ac8c0436d839b33c738463ca2120a003b7a9a
Nicole Lambert <nicole.rose.lambert9@gmail.com>
2022-06-28 18:34:16 -0700

    comments added to js file

--- static/thoughts_buttons.js
+++ static/thoughts_buttons.js
@@ -1,23 +1,23 @@
-
-for (const button of document.querySelectorAll('.save_edits')) {
-    button.addEventListener('click', (evt) => {
+// putting event listeners on each edit button for the thoughts timecapsule
+for (const button of document.querySelectorAll('.save_edits')) { // loops through all buttons with class save_edits
+    button.addEventListener('click', (evt) => {                  // adds event listener
         // evt.preventDefault();
     
-        const formInputs = {
+        const formInputs = {                                     // creates js object with key value 
         journal_id: button.id,
         edits: document.querySelector(`#edit_${button.id}`).value,
         };
     
-        fetch('/journal', {
-        method: 'POST',
-        body: JSON.stringify(formInputs),
+        fetch('/journal', {                                      // makes the request to the journal route
+        method: 'POST',                                          // defines the method the data is being requested
+        body: JSON.stringify(formInputs),                        // turns the json object into a string
         headers: {
             'Content-Type': 'application/json',
         },
         })
-        .then((response) => response.text())
+        .then((response) => response.text())                     // turns the data back into text
         .then((responseJson) => { 
-            alert("Saved!");
+            alert("Saved!");                                     // local host alerts user that edits were saved
             // alert(responseJson.status);
         });
     }); }
